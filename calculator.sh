9b6c6c9698cfd20816bac9b855b4e657192a14da
sedrick <sedrickfuh7@gmail.com>
2023-08-15 15:20:16 +0100

    changes made

--- calculator.sh
+++ calculator.sh
@@ -1,12 +1,33 @@
 #!bin/bash
 #this is a random calculator file
 echo "please calculate below"
-<<<<<<< HEAD
 echo "1.add"
 echo "2. multiply"
 echo "3.divide"
 echo "4.substract"
 echo "choose your option"
 read choice
-=======
->>>>>>> d25904287c4646a401a42dcabb93f0c6395eafa1
+echo "Please enter the first number"
+read num1
+echo "Enter another number"
+read num2
+
+if
+	[ $choice == "1" ]
+then
+	echo $((num1+num2))
+elif 
+	[ $choice == "2" ]
+then
+	echo $((num1*num2))
+elif
+	[ $choice == "3" ]
+then
+	echo $((num1/num2))
+elif
+	[ $choice == "4" ]
+then
+	echo $((num1-num2))
+else 
+	echo "invalid option"
+fi
