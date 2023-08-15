821923748e66146539a4f814024004d8a8e576f2
Emmanuellakulu <emmanuellaefeti85@gmail.com>
2023-08-15 15:40:42 +0100

    This is my calculator

--- calculator.sh
+++ calculator.sh
@@ -1,14 +1,17 @@
 #!bin/bash
-#this is a random calculator file
-echo please calculate below
-<<<<<<< HEAD
-read answer
-expr $answer
-echo "1.add"
-echo "2. multiply"
-echo "3.divide"
-echo "4.substract"
-echo "choose your option"
+#This is a calculator with basic functions
+echo Welcome to my calculator
+echo What operation would you like to perform?
+sleep 5
+echo 1.+
+echo 2./*
+echo 3./
+echo 4.-
+echo choose your option
 read choice
-=======
->>>>>>> d25904287c4646a401a42dcabb93f0c6395eafa1
+echo Enter the first number
+read num1
+echo Enter the second number
+read num2
+echo your answer is `expr $num1 $choice $num2`
+echo Thank you!
