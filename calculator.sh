7c149e3eff1e6eebf038938e58451a4c82412923
sedrick <sedrickfuh7@gmail.com>
2023-08-15 14:59:02 +0100

    changes made

--- calculator.sh
+++ calculator.sh
@@ -1,9 +1,7 @@
 #!bin/bash
 #this is a random calculator file
-echo please calculate below
+echo "please calculate below"
 <<<<<<< HEAD
-read answer
-expr $answer
 echo "1.add"
 echo "2. multiply"
 echo "3.divide"
